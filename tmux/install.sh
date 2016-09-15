fcc8a2405b41437d209b19c2fe89224513e39ba6
Le Tan <tamlokveer@gmail.com>
2016-09-15 13:00:21 +0800

    tmux: add xterm-256color-italic terminfo
    Signed-off-by: Le Tan <tamlokveer@gmail.com>

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 cp .tmux.conf ~/.tmux.conf
+
+tic xterm-256color-italic.terminfo
+echo 'if [ "$TERM" = "xterm-256color" ]; then' >> ~/.bashrc
+echo '    TERM=xterm-256color-italic' >> ~/.bashrc
+echo 'fi' >> ~/.bashrc
