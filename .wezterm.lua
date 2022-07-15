2cc0a8dfbb44f02e9a9bc0b2f290962bdd3731f3
Le Tan <tamlokveer@gmail.com>
2022-07-15 18:03:51 +0800

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,17 +7,43 @@ return {
     check_for_updates = false,
     hide_tab_bar_if_only_one_tab = false,
 
-    -- disable_default_key_bindings = true,
-
     color_scheme = "OneHalfLight",
 
     font = wezterm.font("SauceCodePro Nerd Font"),
     font_size = 13,
 
-    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+    harfbuzz_features = {'calt=0', 'clig=0', 'liga=0'},
     enable_csi_u_key_encoding = true,
 
+    automatically_reload_config = false,
+
+    disable_default_key_bindings = true,
+    leader = {key="a", mods="CTRL", timeout_milliseconds=1000},
     keys = {
-        {key="l", mods="ALT", action=wezterm.action.ShowLauncher},
-    }
+        {key="e", mods="LEADER", action=wezterm.action.ShowLauncher},
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        {key="a", mods="LEADER|CTRL", action=wezterm.action.SendString("\x01")},
+        {key="|", mods="LEADER|SHIFT", action=wezterm.action.SplitHorizontal{domain="CurrentPaneDomain"}},
+        {key="-", mods="LEADER", action=wezterm.action.SplitVertical{domain="CurrentPaneDomain"}},
+        {key="h", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Left")},
+        {key="l", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Right")},
+        {key="z", mods="LEADER", action=wezterm.action.TogglePaneZoomState},
+        {key="c", mods="LEADER", action=wezterm.action.CopyTo("Clipboard")},
+        {key="v", mods="LEADER", action=wezterm.action.PasteFrom("Clipboard")},
+        {key="t", mods="LEADER", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
+        {key="x", mods="LEADER", action=wezterm.action.CloseCurrentTab{confirm=true}},
+        {key="n", mods="LEADER", action=wezterm.action.ActivateTabRelative(1)},
+        {key="p", mods="LEADER", action=wezterm.action.ActivateTabRelative(-1)},
+        {key="1", mods="LEADER", action=wezterm.action.ActivateTab(0)},
+        {key="2", mods="LEADER", action=wezterm.action.ActivateTab(1)},
+        {key="3", mods="LEADER", action=wezterm.action.ActivateTab(2)},
+        {key="4", mods="LEADER", action=wezterm.action.ActivateTab(3)},
+        {key="5", mods="LEADER", action=wezterm.action.ActivateTab(4)},
+        {key="6", mods="LEADER", action=wezterm.action.ActivateTab(5)},
+        {key="7", mods="LEADER", action=wezterm.action.ActivateTab(6)},
+        {key="8", mods="LEADER", action=wezterm.action.ActivateTab(7)},
+        {key="9", mods="LEADER", action=wezterm.action.ActivateTab(8)},
+        {key="f", mods="LEADER", action=wezterm.action.Search{CaseSensitiveString=""}},
+        {key="r", mods="LEADER", action=wezterm.action.ReloadConfiguration},
+    },
 }
