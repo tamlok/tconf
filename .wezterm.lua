5cb603789cf507b41e0268ae576226b6a5e25523
Le Tan <tamlokveer@gmail.com>
2022-11-12 06:08:19 +0800

    [wezterm] refine tab title and color

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,13 @@
 local wezterm = require "wezterm"
 
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = '#3b3486'
     local background = '#624f82'
     local foreground = '#d6e4e5'
 
     if tab.is_active then
-      background = '#6c4ab6'
+      background = '#c47aff'
       foreground = '#ffffff'
     elseif hover then
       background = '#8d72e1'
@@ -21,20 +16,18 @@ wezterm.on(
 
     local edge_foreground = background
 
-    -- ensure that the titles fit in the available space,
-    -- and that we have room for the edges.
-    local title = wezterm.truncate_left(tab.active_pane.title, max_width - 2)
+    -- ensure that the titles fit in the available space
+    local title = tab.active_pane.title
+    space_start, _ = string.find(title, " ")
+    if space_start ~= nil then
+      title = title:sub(0, space_start)
+    end
+    title = wezterm.truncate_left(title, max_width)
 
     return {
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_LEFT_ARROW },
       { Background = { Color = background } },
       { Foreground = { Color = foreground } },
       { Text = title },
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_RIGHT_ARROW },
     }
   end
 )
@@ -105,6 +98,8 @@ return {
         {key="0", mods="LEADER", action=wezterm.action.ActivateLastTab},
         {key="f", mods="LEADER", action=wezterm.action.Search{CaseSensitiveString=""}},
         {key="r", mods="LEADER", action=wezterm.action.ReloadConfiguration},
+        -- CTRL-SHIFT-l activates the debug overlay
+        {key='L', mods='CTRL', action=wezterm.action.ShowDebugOverlay},
     },
 
     mouse_bindings = {
