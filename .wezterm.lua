d2f918182a5c285612266a3c8e45074bfe7aa6f2
Le Tan <tamlokveer@gmail.com>
2024-01-02 14:22:25 +0800

    renew wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,14 @@
 local wezterm = require "wezterm"
 
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
@@ -30,99 +39,99 @@ wezterm.on(
   end
 )
 
-return {
-    -- use NuShell by default
-    default_prog = {"nu"},
-    default_cwd = "",
+-- use NuShell by default
+config.default_prog = {"nu"}
+config.default_cwd = ""
 
-    front_end = "WebGpu",
+config.front_end = "WebGpu"
 
-    window_close_confirmation = "NeverPrompt",
-    check_for_updates = false,
-    hide_tab_bar_if_only_one_tab = false,
+config.window_close_confirmation = "NeverPrompt"
+config.check_for_updates = false
+config.hide_tab_bar_if_only_one_tab = false
 
-    color_scheme = "One Light (base16)",
+config.color_scheme = "One Light (base16)"
 
-    font = wezterm.font("SauceCodePro Nerd Font"),
-    font_size = 13,
+config.font = wezterm.font("SauceCodePro Nerd Font")
+config.font_size = 13
 
-    harfbuzz_features = {'calt=0', 'clig=0', 'liga=0'},
-    enable_csi_u_key_encoding = true,
+config.harfbuzz_features = {'calt=0', 'clig=0', 'liga=0'}
+config.enable_csi_u_key_encoding = true
 
-    use_fancy_tab_bar = true,
+config.use_fancy_tab_bar = true
 
-    window_frame = {
-        font_size = 12.0,
-        active_titlebar_bg = "#888888",
-        inactive_titlebar_bg = "#888888",
-    },
+config.window_frame = {
+    font_size = 12.0,
+    active_titlebar_bg = "#888888",
+    inactive_titlebar_bg = "#888888",
+}
 
-    colors = {
-        tab_bar = {
-            -- The color of the inactive tab bar edge/divider
-            inactive_tab_edge = "#aaaaaa",
-        },
-    },
-
-    inactive_pane_hsb = {
-        saturation = 0.9,
-        brightness = 0.85,
-    },
-
-    launch_menu = {
-        {
-            label = "EdgeTool",
-            args = {"cmd", "/k", "C:\\Edge\\depot_tools\\scripts\\setup\\initEdgeEnv.cmd", "C:\\Edge"},
-            cwd = "C:\\Edge\\src",
-        },
-    },
-
-    automatically_reload_config = false,
-
-    disable_default_key_bindings = true,
-    leader = {key="a", mods="CTRL", timeout_milliseconds=1000},
-    keys = {
-        {key="e", mods="LEADER", action=wezterm.action.ShowLauncher},
-        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-        {key="a", mods="LEADER|CTRL", action=wezterm.action.SendString("\x01")},
-        {key="|", mods="LEADER|SHIFT", action=wezterm.action.SplitHorizontal{domain="CurrentPaneDomain"}},
-        {key="-", mods="LEADER", action=wezterm.action.SplitVertical{domain="CurrentPaneDomain"}},
-        {key="h", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Left")},
-        {key="l", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Right")},
-        {key="z", mods="LEADER", action=wezterm.action.TogglePaneZoomState},
-        {key='{', mods='LEADER|SHIFT', action=wezterm.action.MoveTabRelative(-1)},
-        {key='}', mods='LEADER|SHIFT', action=wezterm.action.MoveTabRelative(1)},
-        {key="c", mods="LEADER", action=wezterm.action.CopyTo("Clipboard")},
-        {key="v", mods="LEADER", action=wezterm.action.PasteFrom("Clipboard")},
-        {key="Insert", mods="SHIFT", action=wezterm.action.PasteFrom("Clipboard")},
-        {key="t", mods="LEADER", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
-        {key="t", mods="CTRL|SHIFT", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
-        {key="x", mods="LEADER", action=wezterm.action.CloseCurrentTab{confirm=true}},
-        {key="n", mods="LEADER", action=wezterm.action.ActivateTabRelative(1)},
-        {key="Tab", mods="CTRL", action=wezterm.action.ActivateTabRelative(1)},
-        {key="p", mods="LEADER", action=wezterm.action.ActivateTabRelative(-1)},
-        {key="Tab", mods="CTRL|SHIFT", action=wezterm.action.ActivateTabRelative(-1)},
-        {key="1", mods="LEADER", action=wezterm.action.ActivateTab(0)},
-        {key="2", mods="LEADER", action=wezterm.action.ActivateTab(1)},
-        {key="3", mods="LEADER", action=wezterm.action.ActivateTab(2)},
-        {key="4", mods="LEADER", action=wezterm.action.ActivateTab(3)},
-        {key="5", mods="LEADER", action=wezterm.action.ActivateTab(4)},
-        {key="6", mods="LEADER", action=wezterm.action.ActivateTab(5)},
-        {key="7", mods="LEADER", action=wezterm.action.ActivateTab(6)},
-        {key="8", mods="LEADER", action=wezterm.action.ActivateTab(7)},
-        {key="9", mods="LEADER", action=wezterm.action.ActivateTab(8)},
-        {key="0", mods="LEADER", action=wezterm.action.ActivateLastTab},
-        {key="f", mods="LEADER", action=wezterm.action.Search{CaseSensitiveString=""}},
-        {key="r", mods="LEADER", action=wezterm.action.ReloadConfiguration},
-        -- CTRL-SHIFT-l activates the debug overlay
-        {key='l', mods='CTRL|SHIFT', action=wezterm.action.ShowDebugOverlay},
-    },
-
-    mouse_bindings = {
-        {
-            event = {Down={streak=1, button="Right"}},
-            mods = '',
-            action = wezterm.action.PasteFrom("Clipboard"),
-        }
+config.colors = {
+    tab_bar = {
+        -- The color of the inactive tab bar edge/divider
+        inactive_tab_edge = "#aaaaaa",
     },
 }
+
+config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.85,
+}
+
+config.launch_menu = {
+    {
+        label = "EdgeTool",
+        args = {"cmd", "/k", "C:\\Edge\\depot_tools\\scripts\\setup\\initEdgeEnv.cmd", "C:\\Edge"},
+        cwd = "C:\\Edge\\src",
+    },
+}
+
+config.automatically_reload_config = false
+
+config.disable_default_key_bindings = true
+config.leader = {key="a", mods="CTRL", timeout_milliseconds=1000}
+config.keys = {
+    {key="e", mods="LEADER", action=wezterm.action.ShowLauncher},
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    {key="a", mods="LEADER|CTRL", action=wezterm.action.SendString("\x01")},
+    {key="|", mods="LEADER|SHIFT", action=wezterm.action.SplitHorizontal{domain="CurrentPaneDomain"}},
+    {key="-", mods="LEADER", action=wezterm.action.SplitVertical{domain="CurrentPaneDomain"}},
+    {key="h", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Left")},
+    {key="l", mods="LEADER", action=wezterm.action.ActivatePaneDirection("Right")},
+    {key="z", mods="LEADER", action=wezterm.action.TogglePaneZoomState},
+    {key='{', mods='LEADER|SHIFT', action=wezterm.action.MoveTabRelative(-1)},
+    {key='}', mods='LEADER|SHIFT', action=wezterm.action.MoveTabRelative(1)},
+    {key="c", mods="LEADER", action=wezterm.action.CopyTo("Clipboard")},
+    {key="v", mods="LEADER", action=wezterm.action.PasteFrom("Clipboard")},
+    {key="Insert", mods="SHIFT", action=wezterm.action.PasteFrom("Clipboard")},
+    {key="t", mods="LEADER", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
+    {key="t", mods="CTRL|SHIFT", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
+    {key="x", mods="LEADER", action=wezterm.action.CloseCurrentTab{confirm=true}},
+    {key="n", mods="LEADER", action=wezterm.action.ActivateTabRelative(1)},
+    {key="Tab", mods="CTRL", action=wezterm.action.ActivateTabRelative(1)},
+    {key="p", mods="LEADER", action=wezterm.action.ActivateTabRelative(-1)},
+    {key="Tab", mods="CTRL|SHIFT", action=wezterm.action.ActivateTabRelative(-1)},
+    {key="1", mods="LEADER", action=wezterm.action.ActivateTab(0)},
+    {key="2", mods="LEADER", action=wezterm.action.ActivateTab(1)},
+    {key="3", mods="LEADER", action=wezterm.action.ActivateTab(2)},
+    {key="4", mods="LEADER", action=wezterm.action.ActivateTab(3)},
+    {key="5", mods="LEADER", action=wezterm.action.ActivateTab(4)},
+    {key="6", mods="LEADER", action=wezterm.action.ActivateTab(5)},
+    {key="7", mods="LEADER", action=wezterm.action.ActivateTab(6)},
+    {key="8", mods="LEADER", action=wezterm.action.ActivateTab(7)},
+    {key="9", mods="LEADER", action=wezterm.action.ActivateTab(8)},
+    {key="0", mods="LEADER", action=wezterm.action.ActivateLastTab},
+    {key="f", mods="LEADER", action=wezterm.action.Search{CaseSensitiveString=""}},
+    {key="r", mods="LEADER", action=wezterm.action.ReloadConfiguration},
+    -- CTRL-SHIFT-l activates the debug overlay
+    {key='l', mods='CTRL|SHIFT', action=wezterm.action.ShowDebugOverlay},
+}
+
+config.mouse_bindings = {
+    {
+        event = {Down={streak=1, button="Right"}},
+        mods = '',
+        action = wezterm.action.PasteFrom("Clipboard"),
+    }
+}
+
+return config
